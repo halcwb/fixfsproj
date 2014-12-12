6b53a94eb52bf578eaf01fb265864375349656b3
halcwb <halcwb@gmail.com>
2014-12-12 20:11:47 +0100

    getting appveyor to run tests

--- test.cmd
+++ test.cmd
@@ -1,4 +1,5 @@
 @echo off
 cls
 if not exist "%~dp0\src\tools\Nunit\nunit-console.exe" "%~dp0\src\.nuget\NuGet.exe" "Install" "Nunit" "-OutputDirectory" "%~dp0src\tools" "-ExcludeVersion"
+if not exist "%~dp0\src\tools\Nunit\nunit-console.exe" "%~dp0\src\.nuget\NuGet.exe" "Install" "NUnit.Runners.Net4" "-OutputDirectory" "%~dp0src\tools" "-ExcludeVersion"
 "%~dp0\src\packages\FAKE\tools\Fake.exe" %~dp0\src\test.fsx
